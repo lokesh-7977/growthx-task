25e755f9eee152e990e184454c33cb0fc9d11429
Lokesh Medharimetla <73935267+lokesh-7977@users.noreply.github.com>
2024-10-09 08:05:05 +0000

    chore:add zod

---
+++ src/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import { config } from './config/index.js'
+import connectDb from './config/db.js'
+
+const app = express()
+const port = config.port;
+
+app.use(cors(
+    {
+        origin: '*',
+        credentials: true,
+    }
+))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+app.get('/', (_req, res) => res.send('Hello!! GrowthX Task Server!'))
+
+
+app.listen(port, () => 
+    connectDb().then(() =>{
+        console.log(`Server is running on http://localhost:${port}`)
+    }))

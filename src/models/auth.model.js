a0d8e1c05e90b96f88ff31094de2be30b72f1a2a
Lokesh Medharimetla <73935267+lokesh-7977@users.noreply.github.com>
2024-10-09 08:25:07 +0000

    feat: add user schema

--- src/models/auth.model.js
+++ src/models/auth.model.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+import { v4 as uuidv4 } from "uuidv4";
+const authSchema = new mongoose.Schema(
+  {
+    id: { type: String, default: () => uuidv4() },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+  },
+  { timestamps: true }
+);
+
+const Auth = mongoose.model("Auth", authSchema);

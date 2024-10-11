f601aa62ca1626f52b4b9c25c3a6c9ecb6361f5e
Lokesh <lokeshchowdary902@gmail.com>
2024-10-11 20:37:31 +0530

    feat:add middleware

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { login, register,getAllAdmins, getAllUsers } from "../controllers/auth.controller.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.post("/register", register);
 router.post("/login", login);
-router.get("/admins",getAllAdmins)
+router.get("/admins",authenticateUser,getAllAdmins)
 router.get("/users",getAllUsers)
 
 export default router;
